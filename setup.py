0bb0eee817eb4583af7b53da71b300637eec6ea0
Luca-Leoni <luca.leoni.99@gmail.com>
2023-03-16 16:33:51 +0100

    Added the installation requirements
    In this way I can opfully run on colab.

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# get the dependencies and installs
+with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
+    all_reqs = f.read().split("\n")
+
+install_requires = [x.strip() for x in all_reqs]
 
 setup(name='NFDMC',
       version='0.0.1',
-      packages=find_packages())
+      packages=find_packages(),
+      install_requires=install_requires)
